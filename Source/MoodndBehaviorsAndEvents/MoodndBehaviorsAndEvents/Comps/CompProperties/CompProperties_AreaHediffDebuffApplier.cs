2c2b7001e9f5cedca7b758e4cac787b2b9e4222b
mlholland <mlholland@users.noreply.github.com>
2022-02-01 22:38:17 -0500

    Refactor AHDA to actually use DGI input
    Change the areaHediffDebuffApplier (the comp used by gibbering mouthers to apply nausea) to actually use the DGI input I added a while back.

--- Source/MoodndBehaviorsAndEvents/MoodndBehaviorsAndEvents/Comps/CompProperties/CompProperties_AreaHediffDebuffApplier.cs
+++ Source/MoodndBehaviorsAndEvents/MoodndBehaviorsAndEvents/Comps/CompProperties/CompProperties_AreaHediffDebuffApplier.cs
@@ -5,7 +5,7 @@ namespace MoodndBehaviorsAndEvents
 {
     public class CompProperties_AreaHediffDebuffApplier : CompProperties
     {
-        // The modified version of the CompProperties_HediffEffecter class from VEF with a few extra params
+        // The modified version of the CompProperties_HediffEffecter class from VEF with a few extra params, as well as a plugin into my own combat utilities
 
         public int radius = 1;
         public int tickInterval = 1000;
@@ -14,14 +14,7 @@ namespace MoodndBehaviorsAndEvents
         public bool requiresPsychicSensitivity = false;
         public bool needsToBeTamed = true;
         public bool affectsSameFaction = true;
-        public float applicationChance = 1f;
-        public int maxTargets = 0; // infinite targets allowed if <= 0 
-        public HediffDef resistanceHediff; // if nonnull, then add this hediff everytime we try to add the main hediff, even if unsuccessful (but not if they're immune for any reason)
-        public float resistanceScaling = 1f;
-        public float resistanceImmunityFloor = 1f;
-        public float resistancePerApplication;
-        public List<HediffDef> immunityHediffs = new List<HediffDef>();
-        public CapacityScalingValues capacityScalingValues = null;
+        public int maxTargetsPerBroadcast = 0; // infinite targets allowed if <= 0  
         public DebuffLogicUtil.DebuffGiverInputs dgi;
          
         public CompProperties_AreaHediffDebuffApplier()
--- Source/MoodndBehaviorsAndEvents/MoodndBehaviorsAndEvents/Comps/Comp_AreaHediffDebuffApplier.cs
+++ Source/MoodndBehaviorsAndEvents/MoodndBehaviorsAndEvents/Comps/Comp_AreaHediffDebuffApplier.cs
@@ -27,98 +27,45 @@ namespace MoodndBehaviorsAndEvents
             //Only do anything every tickInterval
             if (tickCounter > Props.tickInterval)
             {
+                // check that this pawn is able to apply the effect right now according to general and parameterized requirements
                 thisPawn = this.parent as Pawn; 
                 if (thisPawn != null && thisPawn.Map != null && !thisPawn.Dead && !thisPawn.Downed && (!Props.needsToBeTamed || (Props.needsToBeTamed && thisPawn.Faction != null && thisPawn.Faction.IsPlayer)))
                 {
                     int targetsAffected = 0;
+                    // check for potential targets in a radius around the broadcasters
                     foreach (Thing thing in GenRadial.RadialDistinctThingsAround(thisPawn.Position, thisPawn.Map, Props.radius, true))
                     {
-                        Pawn pawn = thing as Pawn; 
-                        //Ensure that this is a valid target by checking a bunch of attributes of the target pawn, some based on the comp config.
+                        Pawn pawn = thing as Pawn;
+                        //Ensure that this thing is a valid target by checking a bunch of attributes of the target pawn, some based on the comp config.
                         if (pawn != null && !pawn.Dead && !pawn.Downed && // basics
                             (!Props.onlyAffectColonists || pawn.IsColonist) && // IsColonist check
                              (!Props.requiresPsychicSensitivity || pawn.GetStatValue(StatDefOf.PsychicSensitivity, true) > 0f) && // psychic check
                              (!Props.affectsSameFaction || this.parent.Faction == null || this.parent.Faction.Equals(pawn.Faction))) // faction check
                         {
-                            
-
-                            float applicationChance = Props.applicationChance;
-                            //
-                            foreach (HediffDef immunityHediffDef in Props.immunityHediffs)
+                            // Roll to hit based on DGI params
+                            if (DebuffLogicUtil.DoesDebuffHappen(pawn, Props.dgi))
                             {
-                                var immunityHediff = pawn.health?.hediffSet?.GetFirstHediffOfDef(immunityHediffDef);
-                                if (immunityHediff != null)
+                                // if hit add the hediff
+                                pawn.health.AddHediff(Props.hediff);
+                                // add/increase resistance if needed
+                                if (Props.dgi.resistanceHediffDef != null)
                                 {
-                                    applicationChance = 0;
-                                }
-                            }
-
-                            // calculate application chance reduction based on resistance hediff if present
-                            if (applicationChance > 0 && Props.resistanceHediff != null)
-                            {
-                                var resistanceHediff = pawn.health?.hediffSet?.GetFirstHediffOfDef(Props.resistanceHediff);
-                                if (resistanceHediff != null)
-                                {
-                                    if (resistanceHediff.Severity >= Props.resistanceImmunityFloor)
+                                    var resistanceHediff = pawn.health?.hediffSet?.GetFirstHediffOfDef(Props.dgi.resistanceHediffDef);
+                                    if (resistanceHediff != null)
                                     {
-                                        applicationChance = 0;
+                                        resistanceHediff.Severity += Props.dgi.resistanceGrowth;
                                     }
                                     else
                                     {
-                                        float resistance = resistanceHediff.Severity * Props.resistanceScaling;
-                                        if (resistance > 1f)
-                                        {
-                                            applicationChance = 0;
-                                        }
-                                        else
-                                        {
-                                            applicationChance *= 1f - resistanceHediff.Severity * Props.resistanceScaling;
-                                        }
+                                        pawn.health.AddHediff(Props.dgi.resistanceHediffDef);
                                     }
                                 }
-                            } 
-
-                            // apply capacity scaling if needed
-                            if (Props.capacityScalingValues != null && applicationChance > 0)
-                            {
-                                applicationChance *= DebuffLogicUtil.PawnCapacityScaling(pawn, Props.capacityScalingValues);
-                            } 
-
-                            // done modifying the chance to hit
-                            if (applicationChance == 0) // if effectively immune
-                            {
-                            // TODO consider adding this back in, but maybe conditionally based on settings or comp flags?
-                                //MoteMaker.ThrowText(pawn.PositionHeld.ToVector3(), pawn.MapHeld, "Immune!", 12f);
-                            }
-                            else // otherwise roll to apply the hediff. Rolling LEC the applicationChance is a hit.
-                            {
-                                // TODO add visuals (probably based on comp settings
-                                float hitRoll = Rand.Value;
-                                if (hitRoll <= applicationChance)
+                                // max targets logic, we only consider a target 'affected' if we rolled to hit them, so immune targets don't add to this count
+                                targetsAffected++;
+                                if (Props.maxTargetsPerBroadcast > 0 && targetsAffected >= Props.maxTargetsPerBroadcast)
                                 {
-                                    pawn.health.AddHediff(Props.hediff);
-                                    if (Props.resistanceHediff != null)
-                                    {
-                                        var resistanceHediff = pawn.health?.hediffSet?.GetFirstHediffOfDef(Props.resistanceHediff);
-                                        if (resistanceHediff != null)
-                                        {
-                                            resistanceHediff.Severity += Props.resistancePerApplication;
-                                        }
-                                        else
-                                        {
-                                            pawn.health.AddHediff(Props.resistanceHediff);
-                                        }
-                                    }
-                                } else if (hitRoll <= Props.applicationChance)
-                                {
-                                    //MoteMaker.ThrowText(pawn.PositionHeld.ToVector3(), pawn.MapHeld, "Resisted!", 12f);
+                                    break;
                                 }
-                            } 
-                            // max targets logic, we only consider a target 'affected' if we rolled to hit them, so immune targets don't add to this count
-                            targetsAffected++;
-                            if (Props.maxTargets > 0 && targetsAffected >= Props.maxTargets)
-                            {
-                                break;
                             }
                         }
                     }
