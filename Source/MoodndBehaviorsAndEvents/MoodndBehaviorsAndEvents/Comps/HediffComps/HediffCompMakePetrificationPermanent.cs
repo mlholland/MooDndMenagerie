df518d007f7871b23d7d04d42b8257c2e4338255
mlholland <mlholland@users.noreply.github.com>
2022-11-17 00:05:09 -0500

    mechs only get fully petrified for 5 days.

--- Source/MoodndBehaviorsAndEvents/MoodndBehaviorsAndEvents/Comps/HediffComps/HediffCompMakePetrificationPermanent.cs
+++ Source/MoodndBehaviorsAndEvents/MoodndBehaviorsAndEvents/Comps/HediffComps/HediffCompMakePetrificationPermanent.cs
@@ -7,6 +7,7 @@ namespace MoodndBehaviorsAndEvents
     {
         public int tickCounter = 0;
         private static string PERMANENT_HEDIFF_NAME = "DND_Petrification_Complete";
+        private static string PERMANENT_HEDIFF_NAME_MECHS = "DND_Petrification_Complete_Mechs";
 
 
         public HediffCompProperties_MakePetrificationPermanent Props
@@ -21,7 +22,8 @@ namespace MoodndBehaviorsAndEvents
         {
             base.CompPostTick(ref severityAdjustment);
             if (this.parent.Severity >= 1) {
-                HediffDef perm = HediffDef.Named(PERMANENT_HEDIFF_NAME);
+                Debug.MooLog("Hello");
+                HediffDef perm = (this.Pawn.RaceProps.IsMechanoid || this.Pawn.RaceProps.IsWorkMech) ? HediffDef.Named(PERMANENT_HEDIFF_NAME_MECHS) : HediffDef.Named(PERMANENT_HEDIFF_NAME);
                 this.Pawn.health.AddHediff(perm);
                 this.Pawn.health.RemoveHediff(this.parent);
             }
